7844021ef92789e5f2e877a52dd9540a3f9da267
Ashmil <ashmilk114@gmail.com>
2024-12-20 12:07:26 +0530

    just commit

--- frontend/src/store/cartStore.ts
+++ frontend/src/store/cartStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { Product } from "../api/product.api";
+import { persist } from "zustand/middleware";
 
 export type Cart = {
   product: Product;
@@ -13,45 +14,52 @@ export type CartStore = {
   clearCart: () => void;
 };
 
-export const useCart = create<CartStore>()((set) => ({
-  cart: [],
-  addItem: (product) => {
-    set((prev) => {
-      const existingItem = prev.cart.find(
-        (item) => item.product.id === product.id
-      );
-      if (existingItem) {
-        existingItem.quantity += 1;
-        return {
-          cart: [
-            ...prev.cart.filter((item) => item.product.id !== product.id),
-            existingItem,
-          ],
-        };
-      }
-      return { cart: [...prev.cart, { product, quantity: 1 }] };
-    });
-  },
-  removeItem: (product) => {
-    set((prev) => {
-      const existingItem = prev.cart.find(
-        (item) => item.product.id === product.id
-      );
-      if (existingItem && existingItem.quantity > 1) {
-        existingItem.quantity -= 1;
-        return {
-          cart: [
-            ...prev.cart.filter((item) => item.product.id !== product.id),
-            existingItem,
-          ],
-        };
-      }
-      return {
-        cart: [...prev.cart.filter((item) => item.product.id !== product.id)],
-      };
-    });
-  },
-  clearCart: () => {
-    set({ cart: [] });
-  },
-}));
+export const useCart = create<CartStore>()(
+  persist(
+    (set) => ({
+      cart: [],
+      addItem: (product) => {
+        set((prev) => {
+          const existingItem = prev.cart.find(
+            (item) => item.product.id === product.id
+          );
+          if (existingItem) {
+            existingItem.quantity += 1;
+            return {
+              cart: [
+                ...prev.cart.filter((item) => item.product.id !== product.id),
+                existingItem,
+              ],
+            };
+          }
+          return { cart: [...prev.cart, { product, quantity: 1 }] };
+        });
+      },
+      removeItem: (product) => {
+        set((prev) => {
+          const existingItem = prev.cart.find(
+            (item) => item.product.id === product.id
+          );
+          if (existingItem && existingItem.quantity > 1) {
+            existingItem.quantity -= 1;
+            return {
+              cart: [
+                ...prev.cart.filter((item) => item.product.id !== product.id),
+                existingItem,
+              ],
+            };
+          }
+          return {
+            cart: [
+              ...prev.cart.filter((item) => item.product.id !== product.id),
+            ],
+          };
+        });
+      },
+      clearCart: () => {
+        set({ cart: [] });
+      },
+    }),
+    { name: "cart" }
+  )
+);
