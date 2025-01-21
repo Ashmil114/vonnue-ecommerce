c7b5df7fdc90cc5bdee584f48f55f01518828161
Ashmil <ashmilk114@gmail.com>
2025-01-21 14:01:20 +0530

    add screenshots

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.12.151']
+ALLOWED_HOSTS = []
 
 
 # Application definition
